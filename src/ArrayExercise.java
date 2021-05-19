2ceb48d64534ec5d96793b080994c23907674e52
Baylie Warrick <bayliewarrick2019@gmail.com>
2021-05-19 10:26:58 -0500

    BW: modify/further complete array loop exercise

--- src/ArrayExercise.java
+++ src/ArrayExercise.java
@@ -1,22 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class ArrayExercise {
 	
 	static int[] myArr = {1,2,3,4,5,6,7,8,9,10};
-	public static int[] emptyArray = new int[10];
+	static List<String> newArr = new ArrayList<String>();;
 
 	public static void print(Object toPrint) {
 		System.out.println(toPrint);
 	}
-	
-	public void populateArray (int lengthToPop) {
-		
-	}
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for(int i = 0; i < myArr.length; i++) {
 			print(myArr[i]);
+			newArr.add(myArr[i] * 10 + "");
 		}
+		print(newArr);
 	}
 
 }
