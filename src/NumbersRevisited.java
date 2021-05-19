2d6c4dbac71b900d030a33ebed468c9f109c6b73
Baylie Warrick <bayliewarrick2019@gmail.com>
2021-05-19 10:20:17 -0500

    added an array exercise. working on numbers exercise

---
+++ src/NumbersRevisited.java
@@ -0,0 +1,14 @@
+
+public class NumbersRevisited {
+
+	public static void makeChange(Double amt) {
+		double originalAmount = amt;
+		double twenties = amount * 20.00
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		makeChange(4.58);
+	}
+
+}
